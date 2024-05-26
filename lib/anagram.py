935ccfac76781af4ac6dcaaf27cc7417af96295e
Daniel Wanjohi <danirunguwanjohi@gmail.com>
2024-05-27 00:12:03 +0300

    Anagrams

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = sorted(word)
+
+    def match(self, possible_anagrams):
+        matches = []
+        for candidate in possible_anagrams:
+            if sorted(candidate) == self.sorted_word:
+                matches.append(candidate)
+        return matches            
+
+listen = Anagram("Listen")
+listen.match(['enlists', 'google', 'inlets', 'banana'])
